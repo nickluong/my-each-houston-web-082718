657f4ab03ff455e5ff879c5b3ba591c2dd5c4869
Nicholas  Luong <nckluong@gmail.com>
2018-08-18 01:13:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each (arr)# put argument(s) here
   # code here
+  count = 0
+  while count < arr.size 
+    yield(arr[count])
+    count += 1 
+  end 
+  arr
 end
